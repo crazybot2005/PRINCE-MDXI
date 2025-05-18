c440c9b6c334269f364dbf0bdf5512f670650a0e
crazybot2005 <theaquaworld94@gmail.com>
2025-05-18 13:45:18 +0530

    Update set.js

--- set.js
+++ set.js
@@ -39,7 +39,8 @@ AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || " powered by prince",
 CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
 WELCOME: process.env.WELCOME || "false",
 GOODBYE: process.env.GOODBYE || "false",    
-AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
+AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "true", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
+AUTO_READ_MESSAGES_EMOJIS: process.env.AUTO_READ_MESSAGES_EMOJIS || "🤍,🧡,💛,🤎,💙,💜,🩷,🩵", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas
 AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799", // Add Multiple Country Codes Separated by Comma...
 PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
 TIME_ZONE: process.env.TIME_ZONE || "Africa/Douala", // Enter yours else leave blank if not sure
